c1ae212fabe33905740d2f6a6c06610302372196
dim42 <dimt777@gmail.com>
2017-01-03 00:09:15 +0300

    +spock test

---
+++ src/test/java/pack1/dw/DWResourceTest.groovy
@@ -0,0 +1,45 @@
+package pack1.dw
+
+import com.fasterxml.jackson.jaxrs.annotation.JacksonFeatures
+import org.glassfish.jersey.client.ClientConfig
+import spock.lang.Specification
+
+import javax.ws.rs.client.Client
+import javax.ws.rs.client.ClientBuilder
+
+import static java.lang.String.format
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON
+import static pack1.dw.DWResource.NAME
+import static pack1.dw.DWResource.PATH
+
+class DWResourceTest extends Specification {
+    void setup() {
+    }
+
+    void cleanup() {
+    }
+
+    def "SayHello"() {
+        def url = PATH + "?" + NAME + "=Name1"
+        print(url)
+
+        expect:
+        a == b
+
+        where:
+        a | b
+        2 | 2
+    }
+
+    def "test get"() {
+        Client client = ClientBuilder.newClient(new ClientConfig().register(JacksonFeatures.class))
+        String uri = format("http://%s:%s/", "localhost", 8080)
+        def path = PATH
+
+        def response = client.target(uri).path(path).queryParam(NAME, "Name1").request(APPLICATION_JSON).get()
+
+        expect:
+        println(response)
+        response.status | 200
+    }
+}
